8b96164c0987f77f615150eaf249556ef7182a2d
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-03-09 10:39:17 +0530

    Initial Commit

---
+++ src/test/java/com/prakhar/practise/di_examples/DiExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakhar.practise.di_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
